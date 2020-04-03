bd5addeaad22bf69ed87ad63d5b990ddf96add7c
TDO_ALTALIS <tdolidon@altalis.fr>
2020-04-03 21:50:25 +0200

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAWnoJQkD9qxJlPhBrFdU3kHrQ7WMP4VTw',
+    authDomain: 'dolitour-of-heroes.firebaseapp.com',
+    databaseURL: 'https://dolitour-of-heroes.firebaseio.com',
+    projectId: 'dolitour-of-heroes',
+    storageBucket: 'dolitour-of-heroes.appspot.com',
+    messagingSenderId: '51749830728'
+  }
 };
